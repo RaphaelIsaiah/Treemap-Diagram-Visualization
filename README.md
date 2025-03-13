1b6962b20b6ca84b568b69e8934a68821bbe2aad
RaphaelIsaiah <rafvie12@gmail.com>
2025-03-13 18:01:24 +0100

    add treemap diagram visualization with D3.js and tooltip functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Treemap Diagram Visualization
 
 ## A freeCodeCamp certification Project
+
+A treemap diagram is used to visualize hierarchical data using nested rectangles, where the size and color of the rectangles represent aspects of the data.
---
+++ script.js
@@ -0,0 +1,102 @@
+const width = 960;
+const height = 600;
+
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+const url =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+
+d3.json(url).then((data) => {
+  // Create treemap layout
+  const treemap = d3.treemap().size([width, height]).padding(1).round(true);
+
+  // Create hierarchy
+  const root = d3
+    .hierarchy(data)
+    .sum((d) => d.value)
+    .sort((a, b) => b.value - a.value);
+
+  treemap(root); // Compute positions
+
+  // Color scale
+  const colorScale = d3
+    .scaleOrdinal()
+    .domain(root.leaves().map((d) => d.data.category))
+    .range(d3.schemeCategory10);
+
+  // Draw tiles
+  const cells = svg
+    .selectAll("g")
+    .data(root.leaves())
+    .enter()
+    .append("g")
+    .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
+
+  cells
+    .append("rect")
+    .attr("class", "tile")
+    .attr("width", (d) => d.x1 - d.x0)
+    .attr("height", (d) => d.y1 - d.y0)
+    .attr("fill", (d) => colorScale(d.data.category))
+    .attr("data-name", (d) => d.data.name)
+    .attr("data-category", (d) => d.data.category)
+    .attr("data-value", (d) => d.data.value);
+
+  // Tooltip
+  const tooltip = d3
+    .select("body")
+    .append("div")
+    .attr("id", "tooltip")
+    .style("opacity", 0);
+
+  cells
+    .on("mouseover", (event, d) => {
+      tooltip.transition().duration(200).style("opacity", 0.9);
+      tooltip
+        .html(
+          `
+      ${d.data.name}<br>
+      Category: ${d.data.category}<br>
+      Value: ${d.data.value}
+    `
+        )
+        .attr("data-value", d.data.value)
+        .style("left", `${event.pageX + 10}px`)
+        .style("top", `${event.pageY - 28}px`);
+    })
+    .on("mouseout", () => {
+      tooltip.transition().duration(500).style("opacity", 0);
+    });
+
+  // Legend
+  const legend = svg
+    .append("g")
+    .attr("id", "legend")
+    .attr("transform", `translate(20, 20)`);
+
+  const categories = [...new Set(root.leaves().map((d) => d.data.category))];
+
+  legend
+    .selectAll("rect")
+    .data(categories)
+    .enter()
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", (d, i) => i * 25)
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("fill", (d) => colorScale(d));
+
+  legend
+    .selectAll("text")
+    .data(categories)
+    .enter()
+    .append("text")
+    .attr("x", 25)
+    .attr("y", (d, i) => i * 25 + 15)
+    .text((d) => d);
+});
